a6a1704b7800b1ebde70569f1528602f420093fe
jessef <1319957+jfrausto@users.noreply.github.com>
2020-07-31 22:51:57 -0700

    local storage saves eventtimes

--- assets/script.js
+++ assets/script.js
@@ -21,18 +21,21 @@ $(document).ready(function () {
   var currentDayDisplay = $("#currentDay");
   currentDayDisplay.text(now);
 
-  var EventTimeList = []; // holds objects; pairs of events and times
+  var timeEventList = []; // holds objects; pairs of events and times
 
-  // function init() {
-  // var storedEventTimeList = JSON.parse(localStorage.getItem("eventTimes"));
+  function init() {
+    var storedEventTimeList = JSON.parse(localStorage.getItem("eventTimes"));
 
-  // if(storedEventTimeList !== null){
-  //   EventTimeList = storedEventTimeList;
-  // }
-  // renderEventTimes();
-  // }
+    if (storedEventTimeList !== null) {
+      timeEventList = storedEventTimeList;
+    }
+    renderEventTimes();
+    console.log(
+      "this is the init function console log of timeEventList: " + timeEventList
+    );
+  }
 
-  // init();
+  init();
 
   function renderEventTimes() {
     // console.log($(".hour").text());
@@ -54,5 +57,38 @@ $(document).ready(function () {
     }
   }
 
-  renderEventTimes();
+  // click save button event
+  $("button").on("click", function (e) {
+    e.preventDefault();
+    var newEvent = $(this).prev().val();
+    console.log("click function: " + newEvent);
+    var hourID = $(this).prev().attr("id");
+    console.log(hourID);
+    // saves event and time into local variable
+
+    // begin storage, create new event object
+    var newTimeEventPair = {
+      event: newEvent,
+      hour: hourID,
+    };
+
+    console.log(
+      "this is the newly created timeEventPair upon CLICK: " +
+        newTimeEventPair.event +
+        "and " +
+        newTimeEventPair.hour
+    );
+    timeEventList.push(newTimeEventPair);
+    // store the scores
+    localStorage.setItem("eventTimes", JSON.stringify(timeEventList));
+    // renderEventTimes();
+  });
+  // press enter after writing event
+  // $("textarea").on("submit", function (e) {
+  //   e.preventDefault();
+  //   var newEvent = $(this).val();
+  //   console.log("submit function: " + newEvent);
+  // });
+
+  // renderEventTimes();
 });
